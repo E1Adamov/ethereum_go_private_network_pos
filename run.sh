1dcc58e61a6a88dae6ddd217f8410cb1a2c5dcdd
Ivan Adamov <psnasti@gmail.com>
2024-07-11 20:06:29 +0200

    initial commit

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
-#docker rm -f $(docker ps -a -q)
-docker compose down; docker system prune --all --force && docker volume prune --force
+docker compose down
 rm -Rf ./consensus/beacondata ./consensus/validatordata ./consensus/genesis.ssz
 rm -Rf ./execution/geth
 docker compose build --no-cache && docker compose up -d
